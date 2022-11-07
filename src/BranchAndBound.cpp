fd7614051bcd56b58c3ca53e8761abc8fe2d0f46
Mateusz Muzioł <matias3004@gmail.com>
2022-11-07 19:32:03 +0100

    Dodanie komentarzy do implementacji

--- src/BranchAndBound.cpp
+++ src/BranchAndBound.cpp
@@ -3,17 +3,19 @@
 #include <queue>
 #include "../inc/BranchAndBound.hh"
 
+// Konstruktor klasy Branch And Bound
 BranchAndBound::BranchAndBound(Graph graph)
 {
-    count = graph.getCount();
-    matrix = graph.getMatrix();
-    uBound = INF;
+    count = graph.getCount(); // Pobranie ilości wierzchołkow w grafie
+    matrix = graph.getMatrix(); // 
+    uBound = INF; // Ustawienie ograniczenia gornego na "nieskończoność"
     tempCost = 0;
-    outgoingEdges = new int[count];
-    visited = new bool[count];
+    outgoingEdges = new int[count]; // Utworzenie tablicy pomocniczej
+    visited = new bool[count]; // Utworzenie tablicy zawierającej listę odwiedzonych wierzchołkow
     n0_lb = 0;
     int min = INF;
 
+    // Wypełnienie tablicy pomocniczej wartościami najnizszych wag krawędzi wychodzących z wierzchołkow
     for (int i = 0; i < count; i++)
     {
         for (int j = 0; j < count; j++)
@@ -26,6 +28,7 @@ BranchAndBound::BranchAndBound(Graph graph)
         min = INF;
     }
 
+    // Oznaczenie wszystkich wierzchołkow jako nieodwiedzonych
     for (int i = 0; i < count; i++)
         visited[i] = false;
 }
@@ -58,64 +61,77 @@ bool operator==(const Node &arg1, const Node &arg2)
 
 void BranchAndBound::recursion(Node node)
 {
-    pathHelper.push(node.vertex);
-    visited[node.vertex] = true;
+    // Dodanie aktualnego wierzchołka do stosu przechowującego ściezkę tymczasową dla danego etapu algorytmu
+    pathHelper.push(node.vertex); 
+    visited[node.vertex] = true; // Oznaczenie aktualnego wierzchołka jako odwiedzonego
     Node temp;
-    std::priority_queue <Node, std::vector<Node>, std::greater<Node>> bounds;
+    // Utworzenie kolejki priorytetowej zawierającej węzły i ich ograniczenia dolne
+    // Najwyzszy priorytet ma wierzchołek o najnizszej wartości ograniczenia dolnego
+    std::priority_queue <Node, std::vector<Node>, std::greater<Node>> bounds; 
 
+    // Sprawdza czy dany węzeł był juz odwiedzony
     for (int i = 0; i < count; ++i)
     {
-        if (!visited[i])
+        if (!visited[i]) // Jeśli nie, to obliczane jest jego ograniczenie dolne
         {
-            temp.vertex = i;
-            temp.lBound = lowerBound(node, i);
-            bounds.push(temp);
+            temp.vertex = i; // Ustawienie aktualnego wierzchołka jako tymczasowy
+            temp.lBound = lowerBound(node, i); // Obliczanie ograniczenia dolnego danego węzła
+            bounds.push(temp); // Dodanie aktualnego węzła wraz z jego ograniczeniem dolnym do kolejki priorytetowej
         }
     }
 
+    // Sprawdzenie czy nie ma więcej wierzchołkow do sprawdzenia
     if (bounds.empty())
     {
+        // Sprawdzenie czy gorne ograniczenie jest większe od dolnego ograniczenia danego wierzchołka
         if (lowerBound(node, 0) < uBound)
         {
-            uBound = lowerBound(node, 0);
-            pathHelper.copy(&path);
+            uBound = lowerBound(node, 0); // Obliczenie dolnego ograniczenia danego wierzchołka i ustawienie go jako ograniczenie gorne
+            pathHelper.copy(&path); // Zapisanie sciezki tymczasowej jako najlepsze znalezione rozwiązanie
         }
     }
-    else
+    else // Jeśli są wierzchołki do sprawdzenia
     {
+        // Powtarzanie algorytmu az nie zostaną sprawdzone wszystkie wierzchołki
         while (!bounds.empty())
         {
+            // Ustawienie wierzchołka o najwyzszym priorytecie (najnizszej wartosci ograniczenia dolnego) jako aktualnie sprawdzany
             temp = bounds.top();
-            bounds.pop();
+            bounds.pop(); // Usuniecie danego wierzchołka ze stosu
+            // Jeśli jego ograniczenie dolne jest mniejsze od gornego ograniczenia powtarza algorytm rozpoczynając od danego wierzchołka
             if (temp.lBound < uBound)
-                recursion(temp);
+                recursion(temp); 
             else
                 break;
         }
     }
 
+    // Usunięcie wierzchołka ze stosu
     pathHelper.pop();
     visited[node.vertex] = false;
 }
 
+// Metoda rozpoczynająca działane algorytmu
 void BranchAndBound::apply()
 {
     Node start;
-    start.vertex = 0;
-    start.lBound = n0_lb;
+    start.vertex = 0; // Ustwaienie korzenia jako pierwszy wierzchołek
+    start.lBound = n0_lb; // Ustawienie ograniczenia dolnego na 0
 
-    recursion(start);
+    recursion(start); // Rozpoczęcie algorytmu w korzeniu grafu
 }
 
+// Metoda obliczająca dolne ograniczenie dla danego wierzchołka
 int BranchAndBound::lowerBound(Node arg, int next)
 {
-    int result = arg.lBound;
-    result -= outgoingEdges[arg.vertex];
-    result += matrix[arg.vertex][next];
+    int result = arg.lBound; // Przekazanie wartości dolnego ograniczenia poprzednika
+    result -= outgoingEdges[arg.vertex]; // Odjęcie wartości najnizszej wagi krawędzi wychodzącej z danego wierzchołka
+    result += matrix[arg.vertex][next]; // Dodanie wartości wagi krawędzi przebytej do następnika
 
     return result;
 }
 
+// Metoda wyświetlająca w konsoli wynik działania algorytmu
 std::string BranchAndBound::toString()
 {
     std::string result = "";
